b8ed8ca530d8843c1f081870d79b02c895f23dea
Alexya Mello <elizabethalexya@gmail.com>
2024-08-23 17:09:09 -0300

    Atualizando o conhecimento de operadores!

--- java-variaveis/src/Operadores.java
+++ java-variaveis/src/Operadores.java
@@ -45,14 +45,48 @@ public class Operadores {
         int numero1 = 1;
         int numero2 = 2;
 
-        boolean simNao = numero1 == numero2;{
+        boolean simNao = numero1 == numero2; {
     
-        if(numero1 == numero2){
+        if(numero1 == numero2) {
             System.out.println("A condição é verdadeira!");
         }
         System.out.println("numeroUm igual a numeroDois?");
 
         }
+        //utilizando a condição Ternária
+        int a, b;
+        a = 5;
+        b = 6;
+
+        if (a == b)
+            System.out.println("Verdadeiro!");
+            else 
+                System.out.println("Falso!");
+
+        String resultado = a == b ? "Verdadeiro!" : "Falso!";
+        System.out.println(resultado);
+        
+        /*textos são armazenados de forma diferente dos números, por isso precisamos do .equals
+        para fazer a comparação de um conteúdo e determinar se é verdadeiro ou falso*/
+        String nomeUm = "Alexya";
+        String nomeDois = new String("Alexya");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+        //aprendendo a utilizar AND e OR
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if(condicao1 && condicao2){
+            System.out.println("As duas condições são verdadeiras!"); //utilizando AND
+        }
+        //para AND, só será verdadeira quando TODAS informações forem verdadeiras
+        if(condicao1 || condicao2){
+            System.out.println("Uma das condições é verdadeira!"); //utilizando OR
+        }
+        //para OR, só será falso quando TODAS informações forem falsas
+        System.out.println("Fim");
+
     }
 
 }
